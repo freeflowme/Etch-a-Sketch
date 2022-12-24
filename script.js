389421faea4fc8c26d9576351fceb970e21fbc66
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-24 13:10:58 -0600

    Complete gridSquare backGround color mouseover effect

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
+//create 256 <div>s for 16x16 grid
 const container = document.querySelector('#container');
-
-for(x>0; x<256; x++) {
+let x
+for(let x = 0; x < 256; x++) {
     const gridSquare = document.createElement('div');
     gridSquare.className = 'gridSquare';
-
-    gridSquare.border = '1px solid black';
-
     document.querySelector('#container').appendChild(gridSquare);
 }
+
+//color <div>s blue upon mouseover
+const gridSquares = document.querySelectorAll('.gridSquare');
+gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener('mouseover', function() {
+        gridSquare.style.backgroundColor = 'blue';
+    })
+})
+
+
+
+
+
+
+//first attempt at code to color all <div>s blue
+/*
+document.querySelector('.gridSquare').addEventListener('mouseover', function() {
+    document.querySelector('.gridSquare').style.backgroundColor = 'blue';
+})
+*/ 
+
+//second attempt at code to color all <div>s blue
+/*
+document.querySelectorAll('.gridSquare').addEventListener('mouseover', mouseOver);
+
+function mouseOver() {
+    document.querySelectorAll('.gridSquare').style.backgroundColor = "blue";
+}
+*/
