a96c745bbe4310ca9bbade636290fa3c49a5ceac
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-26 16:19:51 -0600

    Add button functionality to remove old grid, add new grid, color upon mouseover

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //create 256 <div>s for original 16x16 grid
 const container = document.querySelector('#container');
-let x
+let x;
 for(let x = 0; x < 256; x++) {
     const gridSquare = document.createElement('div');
     gridSquare.className = 'gridSquare';
@@ -15,26 +15,32 @@ gridSquares.forEach(gridSquare => {
     })
 })
 
+//upon button click, prompt user input for new grid size, remove old grid, replace with new
 const newGridBtn = document.querySelector('#newGridBtn');
     newGridBtn.addEventListener('click', () => {
-        const userChoice = prompt("Squres per side in new grid:");
+        const userChoice = prompt("Squres per side in new grid (1-99):");
+        if(isNaN(userChoice)) {
+            alert("Please enter numbers between 1 and 99!");
+        } else if(userChoice <= 1 || userChoice >= 99) {
+            alert("Please keep your entry between 1 and 99!");
+        } else if(userChoice >= 1 && userChoice <= 99) {
+            while(container.firstChild)
+                container.firstChild.remove();
+            let y;
+            for(let y = 0; y < (userChoice * userChoice); y++) {
+                const newGridSquare = document.createElement('div');
+                newGridSquare.className = 'newGridSquare';
+                document.querySelector('#container').appendChild(newGridSquare);
+            const newGridSquares = document.querySelectorAll('.newGridSquare');
+            newGridSquares.forEach(newGridSquare => {
+                newGridSquare.style.width = `${100/userChoice}%`;
+                newGridSquare.style.height = 'auto';
+            })
+            newGridSquares.forEach(newGridSquare => {
+                newGridSquare.addEventListener('mouseover', () => {
+                    newGridSquare.style.backgroundColor = 'red';
+                })
+            })
+            }
+        }   
     })
-
-
-
-
-//first attempt at code to color all <div>s blue
-/*
-document.querySelector('.gridSquare').addEventListener('mouseover', function() {
-    document.querySelector('.gridSquare').style.backgroundColor = 'blue';
-})
-*/ 
-
-//second attempt at code to color all <div>s blue
-/*
-document.querySelectorAll('.gridSquare').addEventListener('mouseover', mouseOver);
-
-function mouseOver() {
-    document.querySelectorAll('.gridSquare').style.backgroundColor = "blue";
-}
-*/
